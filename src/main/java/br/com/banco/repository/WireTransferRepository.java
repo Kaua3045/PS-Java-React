86842395dfdfdd2ff603e3609e2e6d3a4b2e87da
Kauã Pereira <zkauacodes@hotmail.com>
2023-07-15 14:32:17 -0300

    feat(wire-transfer-repository): add jpa specification executor in wire transfer repository

--- src/main/java/br/com/banco/repository/WireTransferRepository.java
+++ src/main/java/br/com/banco/repository/WireTransferRepository.java
@@ -2,8 +2,9 @@ package br.com.banco.repository;
 
 import br.com.banco.entity.WireTransfer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface WireTransferRepository extends JpaRepository<WireTransfer, Long> {
+public interface WireTransferRepository extends JpaRepository<WireTransfer, Long>, JpaSpecificationExecutor<WireTransfer> {
 }
