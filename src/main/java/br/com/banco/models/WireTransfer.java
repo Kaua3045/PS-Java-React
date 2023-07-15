b3b0bef08757cae14a0d1ff0e83de406b62da9b4
Kauã Pereira <zkauacodes@hotmail.com>
2023-07-15 10:19:57 -0300

    feat(wire-transfer): add wire transfer entity

---
+++ src/main/java/br/com/banco/models/WireTransfer.java
@@ -0,0 +1,40 @@
+package br.com.banco.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "transferencia")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class WireTransfer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long wireTransferId;
+
+    @Column(name = "data_transferencia", nullable = false)
+    private LocalDateTime wireTransferDate;
+
+    @Column(name = "valor", nullable = false)
+    private BigDecimal value;
+
+    @Column(name = "tipo", nullable = false)
+    private String type;
+
+    @Column(name = "nome_operador_transacao", nullable = false)
+    private String transactionOperatorName;
+
+    @ManyToOne
+    @JoinColumn(name = "conta_id", nullable = false)
+    private Account account;
+}
