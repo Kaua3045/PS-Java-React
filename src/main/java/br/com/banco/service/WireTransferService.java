bb7d272820fd1dea114a80567588531ea8131436
Kauã Pereira <zkauacodes@hotmail.com>
2023-07-15 15:44:07 -0300

    fix(wire-transfer-service): fix convert local date to local date time

--- src/main/java/br/com/banco/service/WireTransferService.java
+++ src/main/java/br/com/banco/service/WireTransferService.java
@@ -9,6 +9,8 @@ import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -39,15 +41,19 @@ public class WireTransferService {
             List<Predicate> predicates = new ArrayList<>();
 
             if (startDate != null && endDate != null) {
-                predicates.add(criteriaBuilder.between(root.get("data"), startDate, endDate));
+                LocalDateTime start = LocalDateTime.of(startDate, LocalTime.MIN);
+                LocalDateTime end = LocalDateTime.of(endDate, LocalTime.MAX);
+                predicates.add(criteriaBuilder.between(root.get("wireTransferDate"), start, end));
             } else if (startDate != null) {
-                predicates.add(criteriaBuilder.greaterThanOrEqualTo(root.get("data"), startDate));
+                LocalDateTime start = LocalDateTime.of(startDate, LocalTime.MIN);
+                predicates.add(criteriaBuilder.greaterThanOrEqualTo(root.get("wireTransferDate"), start));
             } else if (endDate != null) {
-                predicates.add(criteriaBuilder.lessThanOrEqualTo(root.get("data"), endDate));
+                LocalDateTime end = LocalDateTime.of(endDate, LocalTime.MAX);
+                predicates.add(criteriaBuilder.lessThanOrEqualTo(root.get("wireTransferDate"), end));
             }
 
             if (operatorName != null) {
-                predicates.add(criteriaBuilder.equal(root.get("operador"), operatorName));
+                predicates.add(criteriaBuilder.equal(root.get("transactionOperatorName"), operatorName));
             }
 
             return criteriaBuilder.and(predicates.toArray(new Predicate[0]));
