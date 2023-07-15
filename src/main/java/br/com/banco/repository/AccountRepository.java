bb659f8c3bfcb4ba59f77f615eb3ab6321a568a7
Kauã Pereira <zkauacodes@hotmail.com>
2023-07-15 14:25:48 -0300

    refactor(repository): add repository annotations

--- src/main/java/br/com/banco/repository/AccountRepository.java
+++ src/main/java/br/com/banco/repository/AccountRepository.java
@@ -2,6 +2,8 @@ package br.com.banco.repository;
 
 import br.com.banco.entity.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AccountRepository extends JpaRepository<Account, Long> {
 }
--- src/main/java/br/com/banco/repository/WireTransferRepository.java
+++ src/main/java/br/com/banco/repository/WireTransferRepository.java
@@ -2,6 +2,8 @@ package br.com.banco.repository;
 
 import br.com.banco.entity.WireTransfer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface WireTransferRepository extends JpaRepository<WireTransfer, Long> {
 }
