0646beeb4f6d88aa26268c06ff6c270a99196207
Kauã Pereira <zkauacodes@hotmail.com>
2023-07-15 10:08:26 -0300

    feat(account): add account entity

---
+++ src/main/java/br/com/banco/models/Account.java
@@ -0,0 +1,25 @@
+package br.com.banco.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "conta")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_conta")
+    private Long accountId;
+
+    @Column(name = "nome_responsavel", nullable = false)
+    private String accoutName;
+}
